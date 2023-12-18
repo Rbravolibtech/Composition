c679590bfa9df8564465542127bee15180c2151f
Rbravolibtech <122310916+Rbravolibtech@users.noreply.github.com>
2023-12-18 14:20:08 -0700

    created composition between two classes

--- Program.cs
+++ Program.cs
@@ -1,10 +1,23 @@
-﻿namespace Composition;
+﻿using System;
+
+
+namespace Composition;
+
+
+
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var dbMigrator = new DbMigrator(new Logger());
+
+        var logger = new Logger();
+        var installer = new Installer(logger);
+
+        dbMigrator.Migrate();
+
+        installer.Install();
     }
 }
 
